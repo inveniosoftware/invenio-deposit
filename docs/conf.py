142a4f70deaa3f40d31b0b8b663a6f212447bf08
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2016-06-02 08:43:26 +0200

    docs: sphinx 1.4 upgrade
    Signed-off-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

--- docs/conf.py
+++ docs/conf.py
@@ -27,13 +27,14 @@ from __future__ import print_function
 import os
 
 import sphinx.environment
-from docutils.utils import get_source_line
+
+_warn_node_old = sphinx.environment.BuildEnvironment.warn_node
 
 
-def _warn_node(self, msg, node):
+def _warn_node(self, msg, *args, **kwargs):
     """Do not warn on external images."""
     if not msg.startswith('nonlocal image URI found:'):
-        self._warnfunc(msg, '{0}:{1}'.format(get_source_line(node)))
+        _warn_node_old(self, msg, *args, **kwargs)
 
 sphinx.environment.BuildEnvironment.warn_node = _warn_node
 
