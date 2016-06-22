0320c200ebc1d5c4b5d22c111f33cb9ea2a6025c
Nikos Filippakis <nikolaos.filippakis@cern.ch>
2016-06-22 17:24:11 +0200

    config: angular-schema-form support for ui-select
    * Adds the needed dependencies for ui-select to work.
    
    * Adds a css file for deposit.
    
    * Adds the option to override angular-schema-form templates.
    
    Signed-off-by: Nikos Filippakis <nikolaos.filippakis@cern.ch>

--- invenio_deposit/bundles.py
+++ invenio_deposit/bundles.py
@@ -24,15 +24,31 @@
 
 """UI for Invenio-Deposit."""
 
+from flask_assets import Bundle
 from invenio_assets import NpmBundle
+
+css = Bundle(
+    'node_modules/ui-select/dist/select.css',
+    filters='cleancss',
+    output='gen/deposit.%(version)s.css',
+)
 
 js = NpmBundle(
     'node_modules/angular/angular.js',
     'node_modules/angular-sanitize/angular-sanitize.js',
+    'node_modules/angular-animate/angular-animate.js',
+    'node_modules/angular-strap/dist/angular-strap.js',
+    'node_modules/angular-strap/dist/angular-strap.tpl.js',
+    'node_modules/underscore/underscore-min.js',
+    'node_modules/angular-underscore/index.js',
+    'node_modules/ui-select/dist/select.js',
+    'node_modules/angular-translate/dist/angular-translate.js',
     'node_modules/objectpath/lib/ObjectPath.js',
     'node_modules/tv4/tv4.js',
     'node_modules/angular-schema-form/dist/schema-form.js',
     'node_modules/angular-schema-form/dist/bootstrap-decorator.js',
+    'node_modules/angular-schema-form-dynamic-select/'
+    'angular-schema-form-dynamic-select.js',
     'node_modules/invenio-records-js/dist/invenio-records-js.js',
     'js/invenio_deposit/app.js',
     filters='jsmin',
@@ -40,8 +56,15 @@ js = NpmBundle(
     npm={
         'almond': '~0.3.1',
         'angular': '~1.4.9',
+        'underscore': '~1.8.3',
         'angular-sanitize': '~1.4.9',
+        'angular-animate': '~1.4.8',
+        'angular-strap': '~2.3.9',
+        'angular-underscore': '~0.0.3',
+        'angular-ui-select': 'git://github.com/angular-ui/ui-select#v0.18.0',
+        'angular-translate': '~2.11.0',
         'angular-schema-form': '~0.8.13',
+        'angular-schema-form-dynamic-select': '~0.13.1',
         'invenio-records-js': '~0.0.1',
         'objectpath': '~1.2.1',
         'tv4': '~1.2.7',
